7a17fc9c74133ff14fddf026c9383c834424bc01
Patricia Parker <pk.patricia@gmail.com>
2019-05-06 18:33:05 -0500

    Updated Results

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,12 +25,16 @@ public class Main {
        userTotal = firstUser + secondUser;
        //checkComputerTotal();
        //checkUserTotal();
-       winner = compareTotals();
-       userTotal = firstUser + secondUser;
+
+
+       compareTotals();
        System.out.println("The First User Choice is: " + firstUser);
        System.out.println("The Second User Choice is: " + secondUser);
        System.out.println("The User Total Is: " + userTotal);
        System.out.println("The Systems Total Is: " + computerTotal);
+
+
+
     }
 
 
@@ -76,7 +80,7 @@ public class Main {
     }*/
 
 
-    public static String compareTotals()  throws IOException {
+    public static void compareTotals()  throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str;
@@ -86,20 +90,16 @@ public class Main {
         str = br.readLine();
 
         if (!str.equals("No")) {
-
-            if (firstUser > secondUser) {
-
-                secondUser = getUserAnswer();
-
-
+            if (firstUser >= secondUser) {
+                secondUser = secondUser + getUserAnswer();
             }
 
-            else if (firstUser < secondUser) {
-
-                firstUser = getUserAnswer();
+            else if (firstUser <= secondUser) {
+                firstUser = firstUser + getUserAnswer();
             }
         }
 
+        userTotal = firstUser + secondUser;
 
         if ((userTotal > computerTotal) && (userTotal <= 21)) {
             winner = "User";
@@ -107,14 +107,13 @@ public class Main {
         }
 
         else {
-
             winner = "System";
             System.out.println("The System is the Winner!");
-
         }
 
 
-        return winner;
+
+        return;
 
         }
 
